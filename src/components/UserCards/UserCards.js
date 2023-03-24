32bd4ffbe4cdc732644edfb47b69cb9e7c0b2ad2
Suyash20021999 <suyashmahavirjain@gmail.com>
2023-03-24 22:10:35 +0530

    12: user info resolved

---
+++ src/components/UserCards/UserCards.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function UserCards(props) {
+  const {userData} = props;
+
+
+  const allCards = userData.map( (user) => {
+    return (
+      <Card sx={{ maxWidth: 345 }} key={user.code}>
+    <CardActionArea>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {user.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+    )
+  })
+
+  const allCardsDiv = <div>
+    {allCards}
+  </div>
+  return (
+    {allCardsDiv} 
+    // <div>
+    //   {userData.length > 0 ?: <>No data in User Data</>}
+    // </div>
+  );
+}
